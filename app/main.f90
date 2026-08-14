6184dd889bcbdb929adb35871c5876049724d33f
lidaltonlima <lidaltonlima@gmail.com>
2026-08-13 23:29:56 -0300

    :sparkles: feat: added class Node

--- app/main.f90
+++ app/main.f90
@@ -1,5 +1,10 @@
 program main
+    use EntityNode, only: Node
     implicit none
+    type(Node) :: n1, n2
 
-    print *, 'test'
+    n1 = Node(1d0, 2d0)
+    n2 = Node(4d0, 6d0, .true.)
+
+    print *, n1%distance_to(n2)
 end program
--- src/engineer/entities/physical/EntityNode.f90
+++ src/engineer/entities/physical/EntityNode.f90
@@ -6,5 +6,17 @@ module EntityNode
     type, public :: Node
         real(real64) :: x
         real(real64) :: y
+        logical :: labeled = .false.
+    contains
+        procedure :: distance_to
     end type
+
+contains
+    function distance_to(this, other) result(distance)
+        class(Node), intent(in) :: this
+        class(Node), intent(in) :: other
+        real(real64) :: distance
+
+        distance = sqrt((other%x - this%x)**2 + (other%y - this%y)**2)
+    end function
 end module
